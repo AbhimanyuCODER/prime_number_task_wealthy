1770628bae9a6c624b14be171dc8a3bb6544ef16
Abhimanyu Chauhan <abhimanyu1722000@gmail.com>
2023-12-29 13:57:03 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,69 +1,74 @@
-from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi import FastAPI, HTTPException
 from celery import Celery
 import redis
 import uuid
 
+from pydantic import BaseModel
+
 app = FastAPI()
-database = {}  # Taking manual db
+database = {}  # Manual db
 
-app.config = {
-    "CELERY_BROKER_URL": "redis://localhost:6379/0",
-    "CELERY_RESULT_BACKEND": "redis://localhost:6379/0"
-}
+celery = Celery("main", broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=1)
 
-celery = Celery("app", broker=app.config["CELERY_BROKER_URL"])
-celery.conf.update(app.config)
-redis_client = redis.StrictRedis(host="localhost", port=6379, db=1)
+class User(BaseModel):
+    N: int
 
 
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, int(num**0.5)+ 1):
-        if num%i == 0:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
 
 @celery.task
 def generate_primes(n):
+    print("LEVEL2")
     primes = []
     num = 1
-    while len(primes) <= n:
+    while len(primes) < n:
         if is_prime(num):
             primes.append(num)
         num += 1
     return primes
 
 
-#made this api to post the query to generate the firt n Prime
-
 @app.post("/generate_primes")
-async def generate_primes_number(n: int, background_tasks: BackgroundTasks):
+async def generate_primes_number(i: User):
     request_id = str(uuid.uuid4())
+    print(request_id)
     redis_client.hset(request_id, "status", "pending")
-    background_tasks.add_task(generate_primes_task, request_id, n)
+    generate_primes_task.delay(request_id, i.N)
     return {"request_id": request_id, "status": "pending"}
 
 
+@celery.task
 def generate_primes_task(request_id, n):
+    print("LEVEL")
     result = generate_primes.apply_async(args=[n])
+    print(result)
     update_status.apply_async(args=[request_id, result.id])
 
 
 @celery.task
-def update_status(request_id, result):
-    redis_client.hset(request_id, "status", "processed")
-    redis_client.hset(request_id, "result", result)
+def update_status(request_id, result_id):
+    result = celery.AsyncResult(result_id)
+    if result.ready():
+        redis_client.hset(request_id, "status", "processed")
+        redis_client.hset(request_id, "result", result.get())
+    else:
+        update_status.apply_async(args=[request_id, result_id], countdown=1)
 
 
-#made this api to get the status
 @app.get("/check_status")
 async def check_status(request_id: str):
     status = redis_client.hget(request_id, "status")
     result = redis_client.hget(request_id, "result")
 
     if status:
-        return {"request_id": request_id, "status": status, "result": result}
+        return {"request_id":request_id, "status":status, "result":result}
     else:
-        raise HTTPException(status_code=404, detail="Invalid request")
+        raise HTTPException(status_code=404, detail="invalid request")
